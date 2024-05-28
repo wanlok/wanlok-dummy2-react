33c54e505f8894589ad8cc84cf880778ae3d4364
Robert Wan <wantunglok@gmail.com>
2024-05-28 11:03:52 +1000

    rotate and zoom object by mouse

--- src/FloorPlan.jsx
+++ src/FloorPlan.jsx
@@ -1,6 +1,6 @@
-import * as THREE from "three";
-
 import { useEffect, useRef } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 function FloorPlan() {
   const divRef = useRef(null);
@@ -15,15 +15,22 @@ function FloorPlan() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     // document.body.appendChild( renderer.domElement );
     // use ref as a mount point of the Three.js scene instead of the document.body
+    // var zoom = function (event) {
+    //   camera.position.z += event.deltaY / 240;
+    // };
     if (divRef.current) {
       divRef.current.appendChild(renderer.domElement);
+      // divRef.current.addEventListener("mousewheel", zoom);
     }
     var geometry = new THREE.BoxGeometry(3, 1, 3);
     var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
     var cube = new THREE.Mesh(geometry, material);
     var scene = new THREE.Scene();
     scene.add(cube);
-    camera.position.z = 10;
+    camera.position.z = 8;
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    // controls.touches.ONE = THREE.TOUCH.PAN; controls.touches.TWO = THREE.TOUCH.DOLLY_ROTATE;
     var animate = function () {
       requestAnimationFrame(animate);
       cube.rotation.x += 0.01;
@@ -31,15 +38,16 @@ function FloorPlan() {
       renderer.render(scene, camera);
     };
     animate();
-    var resize = function() {
+    var resize = function () {
       camera.aspect = window.innerWidth / window.innerHeight;
       camera.updateProjectionMatrix();
       renderer.setSize(window.innerWidth, window.innerHeight);
-    }
+    };
     window.addEventListener("resize", resize);
     return () => {
       window.removeEventListener("resize", resize);
       if (divRef.current) {
+        // divRef.current.removeEventListener("mousewheel", zoom);
         divRef.current.removeChild(renderer.domElement);
       }
       material.dispose();

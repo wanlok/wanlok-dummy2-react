0eafd0c75b5666cf4c0a05cd85d37faf46d1860c
Robert Wan <wantunglok@gmail.com>
2024-06-16 16:47:42 +1000

    test api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import "./App.css";
-import FloorPlan from "./FloorPlan";
+
+import React, { useState, useEffect } from 'react';
+
+function GitHubUser({ login }) {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/useres/${login}`)
+      .then((res) => res.json())
+      .then(setData)
+      .catch(console.error);
+  }, []);
+
+  if (data) {
+    return <div>{JSON.stringify(data)}</div>;
+  }
+}
 
 function App() {
   return (
     <>
-      <FloorPlan />
+      <GitHubUser login="wanlok" />
     </>
   );
 }

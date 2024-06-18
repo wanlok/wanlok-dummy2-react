d07a6c06c4275cd83a7d5ab6a6a15ab5f690dfcd
Robert Wan <wantunglok@gmail.com>
2024-06-18 11:10:10 +1000

    separate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,5 @@
 import "./App.css";
 
-import React, { useState, useEffect } from "react";
-
-function GitHubUser({ login }) {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${login}`)
-      .then((res) => {
-        const json = res.json();
-        return json;
-      })
-      .then((data) => {
-        setData(data);
-      })
-      .catch(console.error);
-  }, []);
-  if (data) {
-    return (
-      <div>
-        <h1>{data.login}</h1>
-        <img src={data.avatar_url} width={100} />
-      </div>
-    );
-  }
-}
-
 function App() {
   return (
     <>
---
+++ src/GitHubUser.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+function GitHubUser({ login }) {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then((res) => {
+        const json = res.json();
+        return json;
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch(console.error);
+  }, []);
+  if (data) {
+    return (
+      <div>
+        <h1>{data.login}</h1>
+        <img src={data.avatar_url} width={100} />
+      </div>
+    );
+  }
+}
+
+export default GitHubUser;
